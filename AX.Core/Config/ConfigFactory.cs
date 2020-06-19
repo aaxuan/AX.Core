609b95c8d0354a673f8753bab9aea764daace5eb
aaxuan <xxx>
2020-06-19 15:48:11 +0800

    初步实现config工厂，添加配置类存储到文件方法

---
+++ AX.Core/Config/ConfigFactory.cs
@@ -0,0 +1,55 @@
+﻿using AX.Core.Cache;
+using AX.Core.Extension;
+using System.Collections.Generic;
+
+namespace AX.Core.Config
+{
+    /// <summary>
+    /// 配置工厂 提供全局管理配置的能力
+    /// </summary>
+    internal class ConfigFactory
+    {
+        private static readonly MemoryCache<JsonConfig> _configCache;
+
+        static ConfigFactory()
+        {
+            _configCache = CacheFactory.CreateCache<JsonConfig>("配置实例缓存") as MemoryCache<JsonConfig>;
+        }
+
+        /// <summary>
+        /// 创建并注册配置对象
+        /// </summary>
+        /// <param name="cacheName"></param>
+        public static JsonConfig CreateConfig(string filePath)
+        {
+            filePath.CheckIsNullOrWhiteSpace();
+            var config = new JsonConfig(filePath);
+            _configCache[filePath] = config;
+            return config;
+        }
+
+        /// <summary>
+        /// 获取配置对象
+        /// </summary>
+        /// <param name="cacheName"></param>
+        /// <returns></returns>
+        public static JsonConfig GetCache(string filePath)
+        {
+            filePath.CheckIsNullOrWhiteSpace();
+            if (_configCache.ContainsKey(filePath))
+            {
+                return _configCache[filePath];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// 获取全局所有缓存情况
+        /// </summary>
+        /// <returns></returns>
+        public static List<JsonConfig> GetCaCheList()
+        {
+            return _configCache.AllToList();
+        }
+    }
+}
--- AX.Core/Config/JsonConfig.cs
+++ AX.Core/Config/JsonConfig.cs
@@ -10,31 +10,26 @@ namespace AX.Core.Config
     {
         #region 属性
 
-        private String _configFileNamePath { get; set; }
+        private String _configFileNamePath { get; set; } = DefaultFileNamePath;
 
         private JObject _configJobject { get; set; }
+
+        public static readonly String DefaultFileNamePath = "jsonconfig.json";
 
         #endregion 属性
 
         /// <summary>
         /// 使用文件路径和实体类创建配置实例
-        /// 无文件 名默认文件名 jsonConfig.json
+        /// 无文件 名默认文件名 jsonconfig.json
         /// 无文件会根据类创建默认文件,若type传 null 值，则抛出异常
         /// </summary>
         /// <param name="configFileNamePath"></param>
-        public JsonConfig(string configFileNamePath = null, Type configType = null)
+        public JsonConfig(string configFileNamePath = null)
         {
-            if (configFileNamePath == null)
-            {
-                configFileNamePath = "jsonConfig.json";
-            }
-            _configFileNamePath = configFileNamePath;
+            if (string.IsNullOrWhiteSpace(configFileNamePath) == false)
+            { _configFileNamePath = configFileNamePath; }
             if (File.Exists(configFileNamePath) == false)
-            {
-                if (configType == null)
-                { throw new FileNotFoundException("Json配置未找到配置文件", configFileNamePath); }
-            }
-
+            { throw new FileNotFoundException("Json配置类未找到配置文件", configFileNamePath); }
             //读取配置文件
             var configFileText = File.ReadAllLines(_configFileNamePath);
             var jsonConfigText = new StringBuilder();
@@ -47,7 +42,6 @@ namespace AX.Core.Config
             }
             if (jsonConfigText.Length <= 0)
             { throw new AXWarringMesssageException($"{configFileNamePath}配置文件无有效内容"); }
-
             _configJobject = JObject.Parse(jsonConfigText.ToString());
         }
 
@@ -72,6 +66,14 @@ namespace AX.Core.Config
         public void SetValue(string key, object value)
         {
             _configJobject[key] = JToken.FromObject(value);
+        }
+
+        /// <summary>
+        /// 保存文件
+        /// </summary>
+        public void SaveToFile()
+        {
+            File.WriteAllText(_configFileNamePath, _configJobject.ToString());
         }
     }
 }
