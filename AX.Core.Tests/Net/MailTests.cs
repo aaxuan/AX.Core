7d0aca9918bd30184bfd5357119e79cb9c2ff03b
aaxuan <cuxuan@outlook.com>
2020-07-27 00:03:42 +0800

    同步代码

---
+++ AX.Core.Tests/Net/MailTests.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AX.Core.Net.Tests
+{
+    [TestClass()]
+    public class MailTests
+    {
+        [TestMethod()]
+        public void MailTest()
+        {
+            var mail = new AX.Core.Net.Mail("1051664725@qq.com", "lntgucvqofsgbddj", "Core测试");
+            //mail.Send("1051664725@qq.com", "Core测试", "测试信息,测试信息,测试信息,v,测试信息,v,,v测试信息", false);
+
+            var email = new EMail();
+            //email.Send("1051664725@qq.com", "Core测试", "测试信息,测试信息,测试信息,v,测试信息,v,,v测试信息");
+
+            var email2 = new EMail2();
+            email2.Send("1051664725@qq.com");
+        }
+    }
+}
--- AX.Core/Net/Mail.cs
+++ AX.Core/Net/Mail.cs
@@ -4,6 +4,14 @@ using System.Text;
 
 namespace AX.Core.Net
 {
+    //服务器名称: outlook.office365.com
+    //端口: 993
+    //加密方法: TLS
+
+    //服务器名称: smtp.office365.com
+    //端口: 587
+    //加密方法: STARTTLS
+
     public class Mail
     {
         private readonly SmtpClient _client;
@@ -39,6 +47,71 @@ namespace AX.Core.Net
 
             //发送
             _client.Send(message);
+        }
+    }
+
+    public class EMail
+    {
+        private SmtpClient _client
+        {
+            get
+            {
+                SmtpClient client = new SmtpClient();
+                client.Host = "smtp.qq.com";
+                client.Port = 587;
+                client.EnableSsl = true;
+                client.DeliveryMethod = SmtpDeliveryMethod.Network;
+                client.UseDefaultCredentials = true;
+                client.Credentials = new NetworkCredential(_formAddress, _authCode);
+                return client;
+            }
+        }
+
+        private string _formAddress { get; set; } = "1051664725@qq.com";
+
+        private string _fromName { get; set; } = "AIFund通知";
+
+        private string _authCode { get; set; } = "lntgucvqofsgbddj";
+
+        public void Send(string toAddress, string subject, string body)
+        {
+            var message = new MailMessage();
+            message.SubjectEncoding = Encoding.UTF8;
+            message.BodyEncoding = Encoding.UTF8;
+            message.Priority = MailPriority.High;
+
+            //发送人
+            message.From = new MailAddress(_formAddress, _fromName);
+            //接受人
+            message.To.Add(toAddress);
+            message.Subject = subject;
+            message.Body = body;
+            message.IsBodyHtml = false;
+
+            //发送
+            _client.Send(message);
+        }
+    }
+
+    public class EMail2
+    {
+        public void Send(string email)
+        {
+            SmtpClient client = new SmtpClient("smtp.qq.com");
+            client.EnableSsl = true;
+            client.UseDefaultCredentials = false;
+            client.Credentials = new System.Net.NetworkCredential("1051664725@qq.com", "lntgucvqofsgbddj");
+
+            MailAddress from = new MailAddress("1051664725@qq.com", "显示名称", Encoding.UTF8);//初始化发件人
+
+            MailAddress to = new MailAddress(email, "", Encoding.UTF8);//初始化收件人
+
+            //设置邮件内容
+            MailMessage message = new MailMessage(from, to);
+            message.Body = "asdasdasdasdasd";
+
+            //发送邮件
+            client.Send(message);
         }
     }
 }
