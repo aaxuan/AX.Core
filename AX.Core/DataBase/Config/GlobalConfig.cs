558068eedae39671dc45798676bd94c4c465e71a
aaxuan <cuxuan@outlook.com>
2020-09-18 15:28:01 +0800

    同步代码

--- AX.Core/DataBase/Config/GlobalConfig.cs
+++ AX.Core/DataBase/Config/GlobalConfig.cs
@@ -5,5 +5,7 @@
         public static int CommandTimeout { get; set; } = 50000;
 
         public static bool UseEscapeChar { get; set; } = true;
+
+        public static bool TraceLogSql { get; set; } = true;
     }
 }
--- AX.Core/DataBase/DataRepositories/DapperRepository.cs
+++ AX.Core/DataBase/DataRepositories/DapperRepository.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -53,6 +54,7 @@ namespace AX.Core.DataBase.DataRepositories
         {
             try
             {
+                TraceLog(nameof(InnerExecuteScalar), sql, args);
                 return Connection.ExecuteScalar<T>(sql, GetDynamicParameters(sql, args), Transaction, GlobalConfig.CommandTimeout);
             }
             catch (Exception ex)
@@ -65,6 +67,7 @@ namespace AX.Core.DataBase.DataRepositories
         {
             try
             {
+                TraceLog(nameof(InnerExecuteNonQuery), sql, args);
                 return Connection.Execute(sql, GetDynamicParameters(sql, args), Transaction, GlobalConfig.CommandTimeout);
             }
             catch (Exception ex)
@@ -77,6 +80,7 @@ namespace AX.Core.DataBase.DataRepositories
         {
             try
             {
+                TraceLog(nameof(InnerQueryFirstOrDefault), sql, args);
                 return Connection.QueryFirstOrDefault<T>(sql, GetDynamicParameters(sql, args), Transaction, GlobalConfig.CommandTimeout);
             }
             catch (Exception ex)
@@ -89,6 +93,7 @@ namespace AX.Core.DataBase.DataRepositories
         {
             try
             {
+                TraceLog(nameof(InnerQuerySingleOrDefault), sql, args);
                 return Connection.QuerySingleOrDefault<T>(sql, GetDynamicParameters(sql, args), Transaction, GlobalConfig.CommandTimeout);
             }
             catch (Exception ex)
@@ -101,11 +106,26 @@ namespace AX.Core.DataBase.DataRepositories
         {
             try
             {
+                TraceLog(nameof(InnerQuery), sql, args);
                 return Connection.Query<T>(sql, GetDynamicParameters(sql, args), Transaction, commandTimeout: GlobalConfig.CommandTimeout);
             }
             catch (Exception ex)
             {
                 throw new AXDataBaseException(ex.Message, sql);
+            }
+        }
+
+        internal void TraceLog(string methodname, string sql, params dynamic[] args)
+        {
+            if (GlobalConfig.TraceLogSql)
+            {
+                Trace.WriteLine("----- -----");
+                Trace.WriteLine($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss:fff}] [{methodname}] [{sql}]");
+                var parameters = GetDynamicParameters(sql, args);
+                foreach (var item in parameters.ParameterNames)
+                {
+                    Trace.WriteLine($"[{item}]-->[{parameters.Get<object>(item)}]");
+                }
             }
         }
 
