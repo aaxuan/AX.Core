ea819bf89f6eb7f3713d180934689dc8cfb673e8
Aaxuan <cuxuan@outlook.com>
2021-01-26 17:07:13 +0800

    迁移部分旧代码

---
+++ AX.Core/Network/BrowserClientInfo.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AX.Core.Network
+{
+    class BrowserClientInfo
+    {
+        /// <summary>
+        /// 获取客户端操作系统版本
+        /// HttpContext.Current.Request.UserAgent
+        /// </summary>
+        /// <returns></returns>
+        public static string GetOSName(string userAgent)
+        {
+            if (userAgent.Contains("android"))
+            { return "Android"; }
+            if (userAgent.Contains("mac os x"))
+            { return "ios"; }
+            if (userAgent.Contains("windows phone"))
+            { return "Windows Phone"; }
+            if (userAgent.Contains("nt 10.0"))
+            { return "Windows 10"; }
+            if (userAgent.Contains("NT 6.3"))
+            { return "Windows8.1"; }
+            if (userAgent.Contains("NT 6.2"))
+            { return "Windows8"; }
+            if (userAgent.Contains("nt 6.1"))
+            { return "Windows 7"; }
+            if (userAgent.Contains("nt 6.0"))
+            { return "Windows Vista/Server 2008"; }
+            if (userAgent.Contains("nt 5.2"))
+            { return "Windows Server 2003"; }
+            if (userAgent.Contains("nt 5.1"))
+            { return "Windows XP"; }
+            if (userAgent.Contains("nt 5"))
+            { return "Windows 2000"; }
+            if (userAgent.Contains("nt 4"))
+            { return "Windows NT4"; }
+            if (userAgent.Contains("me"))
+            { return "Windows Me"; }
+            if (userAgent.Contains("98"))
+            { return "Windows 98"; }
+            if (userAgent.Contains("95"))
+            { return "Windows 95"; }
+            if (userAgent.Contains("mac"))
+            { return "Mac"; }
+            if (userAgent.Contains("unix"))
+            { return "UNIX"; }
+            if (userAgent.Contains("linux"))
+            { return "Linux"; }
+            if (userAgent.Contains("sunos"))
+            { return "SunOS"; }
+
+            return "未知";
+        }
+
+        /// <summary>
+        /// 获取浏览器名称
+        /// HttpContext.Current.Request.UserAgent
+        /// </summary>
+        /// <returns></returns>
+        public static string GetBrowser(string userAgent)
+        {
+            if (userAgent.Contains("opera/ucweb"))
+            { return "UC Opera"; }
+            if (userAgent.Contains("openwave/ ucweb"))
+            { return "UCOpenwave"; }
+            if (userAgent.Contains("ucweb"))
+            { return "UC"; }
+            if (userAgent.Contains("360se"))
+            { return "360"; }
+            if (userAgent.Contains("metasr"))
+            { return "搜狗"; }
+            if (userAgent.Contains("maxthon"))
+            { return "遨游"; }
+            if (userAgent.Contains("the world"))
+            { return "世界之窗"; }
+            if (userAgent.Contains("tencenttraveler") || userAgent.Contains("qqbrowser"))
+            { return "腾讯"; }
+            if (userAgent.Contains("chrome"))
+            { return "Chrome"; }
+            if (userAgent.Contains("safari"))
+            { return "Safari"; }
+            if (userAgent.Contains("firefox"))
+            { return "Firefox"; }
+            if (userAgent.Contains("opera"))
+            { return "Opera"; }
+            if (userAgent.Contains("msie"))
+            { return "IE"; }
+
+            return "未知";
+        }
+
+        ///// <summary>
+        ///// 获得客户端IP
+        ///// </summary>
+        ///// <returns></returns>
+        //public static string GetClientIp()
+        //{
+        //    var result = "0.0.0.0";
+        //    if (HttpContext.Current == null && HttpContext.Current.Request == null)
+        //    { return result; }
+        //    result = HttpContext.Current.Request.ServerVariables["HTTP_X_FORWARDED_FOR"];
+        //    if (result == null || result == String.Empty)
+        //    {
+        //        result = HttpContext.Current.Request.ServerVariables["REMOTE_ADDR"];
+        //    }
+        //    if (result == null || result == String.Empty)
+        //    {
+        //        result = HttpContext.Current.Request.UserHostAddress;
+        //    }
+        //    if (result == null || result == String.Empty)
+        //    {
+        //        result = "0.0.0.0";
+        //    }
+        //    return result;
+        //}
+    }
+}
---
+++ AX.Core/Network/SingleHttpRequest.cs
@@ -0,0 +1,120 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace AX.Core.Network
+{
+    public enum HttpMethod
+    {
+        GET,
+        POST,
+    }
+
+    public class SingleHttpRequset
+    {
+        private HttpWebRequest InnerHttpWebRequest;
+        public CookieContainer CookieContainer = new CookieContainer();
+        public Encoding Encoding = GlobalDefaultSetting.Encoding;
+
+        /// <summary>
+        /// 初始化当前请求对象
+        /// url 可不带协议头 自动添加 http:// 协议头
+        /// </summary>
+        /// <param name="httpMethod"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public SingleHttpRequset Init(HttpMethod httpMethod, string url)
+        {
+            //不包含协议头则自动追加 HTTP 协议头
+            if (url.StartsWith("http://", System.StringComparison.CurrentCultureIgnoreCase) == false && url.StartsWith("https://", System.StringComparison.CurrentCultureIgnoreCase) == false)
+            { url = string.Concat("http://", url); }
+            InnerHttpWebRequest = null;
+            InnerHttpWebRequest = WebRequest.Create(url) as HttpWebRequest;
+            InnerHttpWebRequest.Method = httpMethod.ToString();
+            InnerHttpWebRequest.CookieContainer = CookieContainer;
+            InnerHttpWebRequest.UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:84.0) Gecko/20100101 Firefox/84.0";
+            return this;
+        }
+
+        #region 设置属性
+
+        public SingleHttpRequset SetUserAgent(string userAgent)
+        {
+            InnerHttpWebRequest.UserAgent = userAgent;
+            return this;
+        }
+
+        public SingleHttpRequset SetHeader(string name, string value)
+        {
+            InnerHttpWebRequest.Headers.Add(name, value);
+            return this;
+        }
+
+        public SingleHttpRequset SetReferer(string referer)
+        {
+            InnerHttpWebRequest.Referer = referer;
+            return this;
+        }
+
+        #endregion 设置属性
+
+        #region 设置参数
+
+        // content-type 常见的主要有如下3种：
+        //application/x-www-form-urlencoded
+        //multipart/form-data
+        //application/json
+
+        public SingleHttpRequset SetJsonData<T>(T data)
+        {
+            InnerHttpWebRequest.ContentType = "application/json;charset=UTF-8";
+            var value = JsonConvert.SerializeObject(data);
+            var valueBytes = Encoding.GetBytes(value);
+            InnerHttpWebRequest.ContentLength = valueBytes.Length;
+            Stream requsetStream = InnerHttpWebRequest.GetRequestStream();
+            requsetStream.Write(valueBytes, 0, valueBytes.Length);
+            requsetStream.Close();
+            return this;
+        }
+
+        public SingleHttpRequset SetFormData(string data)
+        {
+            //移除换行符
+            data = data.Replace("\r", string.Empty);
+            data = data.Replace("\n", string.Empty);
+            //编码参数
+            //arg = System.Web.HttpUtility.UrlEncode(arg);
+            InnerHttpWebRequest.ContentType = "application/x-www-form-urlencoded";
+
+            var valueBytes = Encoding.GetBytes(data);
+            InnerHttpWebRequest.ContentLength = valueBytes.Length;
+            Stream requsetStream = InnerHttpWebRequest.GetRequestStream();
+            requsetStream.Write(valueBytes, 0, valueBytes.Length);
+            requsetStream.Close();
+            return this;
+        }
+
+        #endregion 设置参数
+
+        public string GetStringResult()
+        {
+            var result = string.Empty;
+            var webResponse = InnerHttpWebRequest.GetResponse() as HttpWebResponse;
+            if (webResponse != null)
+            {
+                using (StreamReader sr = new StreamReader(webResponse.GetResponseStream()))
+                {
+                    result = sr.ReadToEnd();
+                }
+            }
+            return result;
+        }
+
+        public JObject GetJObjectResult()
+        {
+            return JObject.Parse(GetStringResult());
+        }
+    }
+}
