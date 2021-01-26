7153e7cc420f8491070d1dcf810926aa2ec237e0
Aaxuan <cuxuan@outlook.com>
2021-01-26 16:31:36 +0800

    封装 Serilog 替代原有实现日志

--- AX.Core/GlobalDefaultSetting.cs
+++ AX.Core/GlobalDefaultSetting.cs
@@ -5,6 +5,10 @@ namespace AX.Core
 {
     public class GlobalDefaultSetting
     {
+        public static bool IsDebug { get; set; } = false;
+
+        public static bool UseFileLog { get; set; } = false;
+
         public static Encoding Encoding { get; set; } = Encoding.UTF8;
 
         public static BindingFlags BindingFlags { get; set; } = BindingFlags.NonPublic | BindingFlags.Instance | BindingFlags.Public | BindingFlags.Static;
